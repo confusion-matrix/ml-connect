f65a1f6f7f91b8adc67d274fb243fea4c65cfd71
Tiki <michaelwachtel@yahoo.com>
2022-01-22 13:12:42 -0800

    concurrent package.json added.

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -14,10 +14,12 @@ export default function Navbar() {
     // NAV BAR: TODO: ADD LINKS FOR SIGN IN LOGIN
     <div>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-
         {/* THIS IS THE HOME BUTTON, CLICKING THIS SHOULD TAKE A USER TO THE HOMESCREEN */}
         <NavLink className="navbar-brand" to="/">
-          <img style={{ "width": 25 + '%' }} src="https://d3cy9zhslanhfa.cloudfront.net/media/3800C044-6298-4575-A05D5C6B7623EE37/4B45D0EC-3482-4759-82DA37D8EA07D229/webimage-8A27671A-8A53-45DC-89D7BF8537F15A0D.png"></img>
+          <img
+            style={{ width: 25 + "%" }}
+            src="https://d3cy9zhslanhfa.cloudfront.net/media/3800C044-6298-4575-A05D5C6B7623EE37/4B45D0EC-3482-4759-82DA37D8EA07D229/webimage-8A27671A-8A53-45DC-89D7BF8537F15A0D.png"
+          ></img>
         </NavLink>
         <button
           className="navbar-toggler"
@@ -39,8 +41,8 @@ export default function Navbar() {
               <NavLink className="nav-link" to="/create">
                 Create Record
               </NavLink>
-              <NavLink className="nav-link" to="/signup">
-                Sign Up
+              <NavLink className="nav-link" to="/login">
+                Login
               </NavLink>
             </li>
           </ul>
