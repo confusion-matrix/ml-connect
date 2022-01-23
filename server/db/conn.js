3465639f967c88aacb55e49f9c7c4c33fcaada7b
Tiki <michaelwachtel@yahoo.com>
2022-01-23 00:07:43 -0800

    added login to client & server, models, routes, with JWT

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,26 +1,35 @@
-const { MongoClient } = require("mongodb");
+// const { MongoClient } = require("mongodb");
+const mongoose = require("mongoose");
 const Db = process.env.ATLAS_URI;
-const client = new MongoClient(Db, {
+// const mongooseClient = new MongoClient(Db, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
+// var _db;
+
+// module.exports = {
+//   connectToServer: function (callback) {
+//     mongoose.connect(function (err, db) {
+//       // Verify we got a good "db" object
+//       if (db) {
+//         _db = db.db("ml-connect-database");
+//         console.log("Successfully connected to MongoDB.");
+//       }
+//       return callback(err);
+//     });
+//   },
+
+//   getDb: function () {
+//     return _db;
+//   },
+// };
+
+mongoose.connect(Db, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
- 
-var _db;
- 
-module.exports = {
-  connectToServer: function (callback) {
-    client.connect(function (err, db) {
-      // Verify we got a good "db" object
-      if (db)
-      {
-        _db = db.db("ml-connect-database");
-        console.log("Successfully connected to MongoDB."); 
-      }
-      return callback(err);
-         });
-  },
- 
-  getDb: function () {
-    return _db;
-  },
-};
+
+module.exports = mongoose.connection;
