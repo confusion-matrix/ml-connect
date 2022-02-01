8e8d570c83f3228508913f587e2625876c9c6483
Tiki <michaelwachtel@yahoo.com>
2022-01-31 22:05:52 -0800

    fixed userpage login routing

--- client/src/App.js
+++ client/src/App.js
@@ -13,25 +13,28 @@ import Edit from "./components/SampleEdit";
 import Create from "./components/SampleCreate";
 import Footer from "./components/footer";
 import RecordList from "./components/SampleRecordList";
-import "./components/style.css"
+import "./components/style.css";
 
 // import UploadData from "./components/uploadData";
 import UserPage from "./components/userPage";
+import Auth from "./utils/auth";
 
 const App = () => {
-  const myStyle= {
-    background: 'radial-gradient(circle, rgba(255,255,255,1) 0%, rgba(242,242,242,1) 62%, rgba(201,201,201,1) 100%)'
-  }
+  const myStyle = {
+    background:
+      "radial-gradient(circle, rgba(255,255,255,1) 0%, rgba(242,242,242,1) 62%, rgba(201,201,201,1) 100%)",
+  };
   return (
     <div style={myStyle}>
       <AppNavbar />
       <Routes>
-        <Route exact path="/" element={<HomePage />} />
-        <Route path="/edit/:id" element={<Edit />} />
-        <Route path="/create" element={<Create />} />
-        
-        
-        <Route path="/userPage" element={<UserPage />} />
+        {Auth.loggedIn() ? (
+          <Route path="/" element={<UserPage />} />
+        ) : (
+          <Route exact path="/" element={<HomePage />} />
+        )}
+        {/* <Route path="/edit/:id" element={<Edit />} />
+        <Route path="/create" element={<Create />} /> */}
       </Routes>
       <Footer />
     </div>
